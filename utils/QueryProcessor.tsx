f2de244ff19d7aaba707b7411db062a441e9048a
Allison Feldman <abf2@andrew.cmu.edu>
2024-03-14 15:54:48 -0400

    why

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,12 +11,6 @@ export default function QueryProcessor(query: string): string {
       "abf2"
     );
   }
+
   return "";
-  // else if (query.toLowerCase().includes("name")) {
-  //   return (
-  //     "My name is Allie"
-  //   );
-  // }
-
-
 }
