8a5c7b86bb9f428c6a49236f94c2cf11f7a0f31c
Allison Feldman <abf2@andrew.cmu.edu>
2024-03-14 16:14:09 -0400

    largest

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,4 +1,23 @@
-export default function QueryProcessor(query: string) {
+function findLargestNumber(numbers: number[]): number {
+  var largest = numbers[0];
+  for (var i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+  return largest;
+}
+
+
+export default function QueryProcessor(query: string):string {
+  var words = query.replace(',', '').split(' ');
+  var numbers: number[] = [];
+  for (var i = 0; i < words.length; i++) {
+    var parsedNumber = parseInt(words[i]);
+    if (!isNaN(parsedNumber)) {
+      numbers.push(parsedNumber);
+    }
+  }
   if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
@@ -15,6 +34,9 @@ export default function QueryProcessor(query: string) {
    if (query.toLowerCase().includes('50')) {
     return ('the sum of 50 and 9 is 59')
    }
+  }
+  else if (query.toLowerCase().includes('largest')){
+    return ('the largest of those numbers is ' + findLargestNumber(numbers))
   }
 
   return ("");
